bac07bb013d59c5a080741f3085cb2c8d5429494
Felice Pollano <fpollano@ors.local>
2019-12-05 09:35:54 +0100

    new controller

---
+++ src/Pokespeare/Controllers/PokemonController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Pokespeare.Controllers
+{
+    [Route("/[controller]")]
+    [ApiController]
+    public class PokemonController : ControllerBase
+    {
+        // GET /pokemon/name
+        [HttpGet("{pokemon}")]
+        public ActionResult<string> Get(string pokemon)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
